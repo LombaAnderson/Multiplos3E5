f1681a5a438fbdf7aa13be10d9e36bfbb242aa81
Anderson Lomba <andersonlomba172@gmail.com>
2022-08-19 19:17:40 -0300

    feat: Update

--- Multiplos3E5/Program.cs
+++ Multiplos3E5/Program.cs
@@ -1,51 +1,44 @@
 ﻿
 public class Program
-{
-   
+{   
     public static void Main()
     {
-        
-        Somar();
+        Console.Clear();
 
-        static void Somar()
+        Console.WriteLine("Seja bem vindo aos múltiplos de 3 e 5");
+
+        Console.WriteLine("Digite o primeiro número");
+        int n1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número");
+        int n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($" O resultado da soma dos múltiplos é ");
+
+        SomaMultiplos();
+    }
+         static void SomaMultiplos()
+            { 
+          int numero = 0;
+
+        while (numero <= 100)
         {
-            Console.Clear();
-
-            Console.WriteLine("Seja bem vindo aos múltiplos de 3 e 5");
-
-
-            Console.WriteLine(" Digite o primeiro número");
-            int numero1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o segundo número");
-            int numero2 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("");
-
-            int resultado = numero1 + numero2;
-            Console.WriteLine($" O resultado da soma dos múltiplos é " + resultado);
-            Console.WriteLine(numero1);
-
-            int numero = 0;
-
-
-            while (numero <= 100)
+            numero++;
+            if (numero % 3 == 0  || numero % 5 == 0)
             {
-                numero++;
-                if (numero % 3 == 0)
-                {
-                    Console.WriteLine(numero);
-                }
-                if (numero % 5 == 0)
-                {
-                    Console.WriteLine(numero);
-                }
+                Console.WriteLine(numero);
             }
-            Console.ReadKey();
+
         }
+           Console.ReadKey();
 
     }
-}
+            
+    }
+     
+ 
+     
+
 
    
 
