06f0a40aca94968b9b77d481ed535b99d73bcb44
Anderson Lomba <andersonlomba172@gmail.com>
2022-08-19 15:29:56 -0300

    feat: Update

--- Multiplos3E5/Program.cs
+++ Multiplos3E5/Program.cs
@@ -4,10 +4,10 @@ Console.Clear();
 Console.WriteLine("Seja bem vindo aos múltiplos de 3 e 5");
 
 
-Console.WriteLine(" Digite o primeiro número entre 1 e 10");
+Console.WriteLine(" Digite o primeiro número entre 1 e 100");
 int numero1 = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Digite o segundo número entre 1 e 10");
+Console.WriteLine("Digite o segundo número entre 1 e 100");
 int numero2 = int.Parse(Console.ReadLine());
 
 
@@ -20,7 +20,7 @@ int resultado = 0;
 
 
 
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < 100; i++)
 {
     if (i % multiplo3 == 0 || i % multiplo5 == 0)
     {
@@ -28,7 +28,7 @@ for (int i = 0; i < 10; i++)
     }
 }
 
-Console.WriteLine("A soma dos múltiplos de 3 e 5 abaixo de 10 é: " + resultado);
+Console.WriteLine("A soma dos múltiplos de 3 e 5 abaixo de 100 é: " + resultado);
 
 
 
