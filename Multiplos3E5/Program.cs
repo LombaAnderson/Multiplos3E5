84e0833b8d3deed4db8a66056a41f6de2a90275f
Anderson Lomba <andersonlomba172@gmail.com>
2022-08-19 16:54:58 -0300

    feat: Update

--- Multiplos3E5/Program.cs
+++ Multiplos3E5/Program.cs
@@ -1,35 +1,57 @@
 ﻿
-Console.Clear();
-
-Console.WriteLine("Seja bem vindo aos múltiplos de 3 e 5");
-
-
-Console.WriteLine(" Digite o primeiro número entre 1 e 100");
-int numero1 = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Digite o segundo número entre 1 e 100");
-int numero2 = int.Parse(Console.ReadLine());
-
-
-Console.WriteLine("");
-
-
-int multiplo3 = 3;
-int multiplo5 = 5;
-int resultado = 0;
-
-
-
-for (int i = 0; i < 100; i++)
+public class Program
 {
-    if (i % multiplo3 == 0 || i % multiplo5 == 0)
+    public static void Main()
     {
-        resultado += i;
+        Console.Clear();
+
+        Console.WriteLine("Seja bem vindo aos múltiplos de 3 e 5");
+
+
+        Console.WriteLine(" Digite o primeiro número");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número");
+        int numero2 = int.Parse(Console.ReadLine());
+
+
+        Somar();
+
+        Console.WriteLine("");
+
+        static void Somar()
+        {
+
+            Somar();
+
+
+            Console.WriteLine("");
+
+            static void Somar()
+            {
+                int numero = 0;
+                int n1;
+                int n2;
+                int resultado;
+
+                while (numero <= 100)
+                {
+                    numero++;
+                    if (numero % 3 == 0)
+                    {
+                        Console.WriteLine(numero);
+                    }
+                    if (numero % 5 == 0)
+                    {
+                        Console.WriteLine(numero);
+                    }
+
+                }
+                Console.ReadKey();
+
+            }
+
+        }
+
     }
 }
-
-Console.WriteLine("A soma dos múltiplos de 3 e 5 abaixo de 100 é: " + resultado);
-
-
-
-
