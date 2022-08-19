8ed528d4a6aefb997e8e6b14fee9bfe3ff6b7625
Anderson Lomba <andersonlomba172@gmail.com>
2022-08-19 17:10:41 -0300

    feat: Update

--- Multiplos3E5/Program.cs
+++ Multiplos3E5/Program.cs
@@ -1,57 +1,51 @@
 ﻿
 public class Program
 {
+   
     public static void Main()
     {
-        Console.Clear();
-
-        Console.WriteLine("Seja bem vindo aos múltiplos de 3 e 5");
-
-
-        Console.WriteLine(" Digite o primeiro número");
-        int numero1 = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Digite o segundo número");
-        int numero2 = int.Parse(Console.ReadLine());
-
-
+        
         Somar();
-
-        Console.WriteLine("");
 
         static void Somar()
         {
+            Console.Clear();
 
-            Somar();
+            Console.WriteLine("Seja bem vindo aos múltiplos de 3 e 5");
 
+
+            Console.WriteLine(" Digite o primeiro número");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            int numero2 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("");
 
-            static void Somar()
+            int resultado = numero1 + numero2;
+            Console.WriteLine($" O resultado da soma dos múltiplos é " + resultado);
+            Console.WriteLine(numero1);
+
+            int numero = 0;
+
+
+            while (numero <= 100)
             {
-                int numero = 0;
-                int n1;
-                int n2;
-                int resultado;
-
-                while (numero <= 100)
+                numero++;
+                if (numero % 3 == 0)
                 {
-                    numero++;
-                    if (numero % 3 == 0)
-                    {
-                        Console.WriteLine(numero);
-                    }
-                    if (numero % 5 == 0)
-                    {
-                        Console.WriteLine(numero);
-                    }
-
+                    Console.WriteLine(numero);
                 }
-                Console.ReadKey();
-
+                if (numero % 5 == 0)
+                {
+                    Console.WriteLine(numero);
+                }
             }
-
+            Console.ReadKey();
         }
 
     }
 }
+
+   
+
